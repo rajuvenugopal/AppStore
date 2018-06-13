ca6bb1c801227fb3733737aa8d0e4a1a83958cac
abhisinghcgi <40200417+abhisinghcgi@users.noreply.github.com>
2018-06-13 16:13:56 +0530

    Update demo.component.ts

--- client/src/app/demo/demo.component.ts
+++ client/src/app/demo/demo.component.ts
@@ -1,6 +1,10 @@
+import { Subscription } from 'rxjs/Subscription';
+import { EditComponent } from './../edit/edit.component';
 import { DataserviceService } from './../service/dataservice.service';
 import { Component, OnInit } from '@angular/core';
 import { Router ,ActivatedRoute} from '@angular/router';
+import { AuthService } from './../service/auth.service';
+import { AppStore } from './../models/AppStore';
 
 @Component({
   selector: 'app-demo',
@@ -12,13 +16,42 @@ export class DemoComponent implements OnInit {
   private sub: any;
   products;
 
+  subscription: Subscription;  
+  appfilter:AppStore[]; 
+  copyapps:AppStore[]; //for taking the appstore data
+  dashfilter:AppStore[];
+  filteredproducts:any[]=[];
+  categoryproducts:any[]=[];
+  Categories = ['Finance' , 'AI' , 'Telecom' ,
+  'Block Chain' , 'eductaion' ,'gamification'];
+  displayMode=1;
+  isActive:true;
+
+
+
   constructor(private route: ActivatedRoute, 
               private router: Router,
-              private service : DataserviceService) { }
+              private service : DataserviceService,
+              private authService: AuthService) {
+
+                this.subscription= this.service.getApps()
+                .subscribe(response => {
+                  this.filteredproducts= this.copyapps = this.appfilter =
+                   response.json();
+                 console.log(response.json);
+                }); 
+
+               }
 
 // navigatetoedit(){
 // 	 this.router.navigate(['/edit']);
 // }
+
+filter(query:string){
+  this.filteredproducts=(query) ?
+  this.appfilter.filter(item=> item.name.toLowerCase()
+  .includes(query.toLowerCase())) : this.appfilter;
+}
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
